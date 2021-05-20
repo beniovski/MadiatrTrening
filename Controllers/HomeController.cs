a4dd2001e48098cbbfe11a666f55a324d0aee3fd
daniel daniel <danbed19905@gmail.com>
2021-05-20 22:05:55 +0200

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadiatrTests.Controllers
+{
+    public class HomeController: BaseController
+    {
+
+        [HttpGet]
+        public ActionResult UserOrders()
+        {
+
+            return Ok("ok");
+        }
+
+
+    }
+}
